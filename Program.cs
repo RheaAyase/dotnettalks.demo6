37510e72ca11f4dfa41f100e75804c0782e0000b
Radka Janek <radka.janek@redhat.com>
2018-04-04 00:37:28 +0200

    touch

--- Program.cs
+++ Program.cs
@@ -9,14 +9,14 @@ namespace AsyncBasic
 	{
 		static async Task Main(string[] args)
 		{
-			//Console.WriteLine("Runnig object-oriented code example...\n");
-			//BottleDemo.Run();
+			Console.WriteLine("Runnig object-oriented code example...\n");
+			BottleDemo.Run();
 
 			//Console.WriteLine("Running async code time test...\n");
 			//await AsyncDemo.RunTest();
 
-			Console.WriteLine("Running async code example...\n");
-			await AsyncDemo.Run();
+			//Console.WriteLine("Running async code example...\n");
+			//await AsyncDemo.Run();
 		}
 	}
 }
