ff86a9387787dceb478d284d5f7fb018333260a2
Radka Janek <radka.janek@redhat.com>
2018-04-10 19:26:37 +0200

    Added comments and tweaked the async example

--- AsyncBasic.cs
+++ AsyncBasic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Net.Http;
 
 namespace AsyncBasic
 {
@@ -12,19 +13,47 @@ namespace AsyncBasic
         const int MaxPrimeToPrint = 32452843;
         const int ThreadsToUse = 4;
 
+        const string ExampleUrl = "http://fedoraloves.net";
+
         public static async Task Run()
         {
+            // Start concurrent work on background threads (thread pool.) using `Task.Run()`
             Task printTask = PrimeNumbers.PrintPrimeNumbersConcurrent(MaxPrimeToPrint, ThreadsToUse);
 
-            Console.WriteLine($"Looking for the first prime number higher than {ExamplePrime} *asynchronously* (1 main thread) " +
-                              $"while also printing all the prime numbers up to {MaxPrimeToPrint} " +
-                              $"*concurrently* ({ThreadsToUse} different threads) ...");
+            Console.WriteLine($"Started to print all the prime numbers up to {MaxPrimeToPrint} " +
+                              $"*concurrently* (using up to {ThreadsToUse} different threads) ...");
 
-            int foundPrime = await PrimeNumbers.PrintPrimeNumbersAndGetNextAsync(ExamplePrime, false);
+
+            // Start asynchronous I/O task on the main thread by awaiting an `async` method.
+            Console.WriteLine("=== HttpClient().GetStringAsync() started! ===");
+            Task<string> webTask = new HttpClient().GetStringAsync(ExampleUrl);
+
+            Console.WriteLine($"Started to download web content from {ExampleUrl} *asynchronously* (on the main thread) ...");
+
+
+            Console.WriteLine($"While waiting for the website data, we can calculate what would be the " +
+                              $"next prime number after {ExamplePrime} fully *synchronously* (blocking the main thread) ...");
+
+            // Start an async Task and run it completely synchronously blocking the main thread - Avoid doing this!
+            Task<int> primeTask = PrimeNumbers.PrintPrimeNumbersAndGetNextAsync(ExamplePrime, false);
+            int foundPrime = primeTask.Result;
+            //int foundPrime = primeTask.GetAwaiter().GetResult(); // This is incorrect use for us, as the GetAwaiter
+            // is meant to be used only by the compiler and not by the programmer.
+            // However it will achieve the same thing - get a result of the asynchronous method, synchronously,
+            // and they both block the main thread. Avoid using `.Result` as well, and correctly `await` everything :]
 
             Console.WriteLine($"Found the prime number: {foundPrime}");
 
+
+            // Wait for the I/O task to finish, and get it's result.
+            string webContent = await webTask;
+            Console.WriteLine("=== HttpClient().GetStringAsync() done! ===");
+            Console.WriteLine($"Downloaded web content (1st line): {webContent.Split('\n')[0]}"); //writing only the first line...
+
+
+            // Wait for the concurrent CPU task to finish (it does not have a result.)
             await printTask;
+
 
             Console.WriteLine($"All done!");
         }
@@ -33,13 +62,20 @@ namespace AsyncBasic
         {
             Console.WriteLine($"Looking for {PrimeToFind} prime numbers *synchronously* (1 main thread) ...");
             Stopwatch watch = Stopwatch.StartNew();
+
+            // Synchronous code.
             int prime = PrimeNumbers.FindNthPrime(PrimeToFind);
+
             watch.Stop();
             Console.WriteLine($"Synchronously found the {PrimeToFind}th prime number {prime} in {watch.ElapsedMilliseconds}ms");
 
+
             Console.WriteLine($"Looking for {PrimeToFind} prime numbers *concurrently* ({ThreadsToUse} threads) ...");
             watch.Restart();
+
+            // Immediately awaited asynchronous code executing multiple threads concurrently.
             await PrimeNumbers.PrintPrimeNumbersConcurrent(prime, ThreadsToUse);
+
             watch.Stop();
             Console.WriteLine($"Concurrently found all the prime numbers up to {prime} in {watch.ElapsedMilliseconds}ms");
         }
@@ -59,8 +95,13 @@ namespace AsyncBasic
                     continue;                            // ...would never find a single prime number at all.
                 tasks.Add(Task.Run(() => PrintPrimeNumbersAndGetNextAsync(max, true, threads, i)));
             }
+            // Executing PrintPrimeNumbersAndGetNextAsync on a "threadpool" - on some pre-allocated thread, in a loop.
+            // In order to check all the numbers from 1 to infinity, by using, for example 4 `threads`,
+            // we have to somehow separate this list of numbers. We can do so by the number of threads,
+            // simply by starting each thread with an offset of it's number, and then incrementing the tested number
+            // by the total number of threads.
 
-            await Task.WhenAll(tasks);
+            await Task.WhenAll(tasks);    // Yields until all of the threads are finished.
             Console.WriteLine("=== PrintPrimeNumbersConcurrent done! ===");
         }
 
